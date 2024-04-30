447855f0b810ab23087308e5fc115ec11714d2b5
FelipeBSPereira <felipebsp@al.insper.edu.br>
2024-04-30 12:11:53 -0300

    funcao aloca_navios

--- aloca.py
+++ aloca.py
@@ -14,18 +14,24 @@ def posicao_suporta(m, b, l, c, orient_aloc):
             if m[l][c+i] != ' ':
                 return False
     return True
+import random
 
-def aloca_navios(m, b):
+def aloca_navios(m, l_b):
     tam = len(m)
-    linha = random.randint(0, n-1)
-    coluna = random.randint(0, n-1)
-    orientacao = random.choice(['h', 'v'])
-    for i in b:
-        pode_alocar = posicao_suporta(m,b,l,c,orient_aloc)
+    for tam_n in l_b:
+        l = random.randint(0, tam-1)
+        c = random.randint(0, tam-1)
+        orient_aloc = random.choice(['h', 'v'])
+        pode_alocar = posicao_suporta(m, tam_n, l, c, orient_aloc)
         while pode_alocar == False:
-            linha = random.randint(0, n-1)
-            coluna = random.randint(0, n-1)
-            orientacao = random.choice(['h', 'v'])
-            pode_alocar = posicao_suporta(m,b,l,c,orient_aloc)
-            
-
+            l = random.randint(0, tam-1)
+            c = random.randint(0, tam-1)
+            orient_aloc = random.choice(['h', 'v'])
+            pode_alocar = posicao_suporta(m, tam_n, l, c, orient_aloc)
+        if orient_aloc == 'v':
+            for i in range(l, l+tam_n):
+                m[i][c] = 'N'
+        elif orient_aloc == 'h':
+            for j in range(c, c+tam_n):
+                m[l][j] = 'N'
+    return m
